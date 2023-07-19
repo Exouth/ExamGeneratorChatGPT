e41e9900a363d3cd5714226ac172d69fdeaad4c7
Exouth <Exouth@protonmail.com>
2023-07-19 13:47:17 +0200

    Added Shebang for Environment and KeyboardInterrupt Exit

--- src/examgenerator.py
+++ src/examgenerator.py
@@ -1,3 +1,5 @@
+#!/media/marcel/2TBSSD/Coding/ExamGeneratorChatGPT/env/bin/python
+
 import sys
 sys.dont_write_bytecode = True
 
@@ -7,25 +9,29 @@ from odf.text import P
 
 engine = ExamPrompt("180bdc1f34ea46a48abaf34f2c121cb05391263a49a1b3c246c6241df172bf8dc06dd27d4c0b8edbc60eed6380be8b27566c6bcd7dfa34578efe5e0113605839")
 
+try:
 
-name = engine.ask_exam_name()
+    name = engine.ask_exam_name()
 
-engine.ask_topic_number()
+    engine.ask_topic_number()
 
-toppic_notes = engine.give_topic_notes()
+    toppic_notes = engine.give_topic_notes()
 
-engine.next_step()
-
-engine.give_initial_Exam_Message()
-
-engine.next_step()
-
-doc = OpenDocumentText()
-for topic in toppic_notes:
-    page, note_id, title = topic
-    engine.prompt_exam(note_id, doc)
     engine.next_step()
 
-doc.save(name);
+    engine.give_initial_Exam_Message()
 
-engine.exit()
+    engine.next_step()
+
+    doc = OpenDocumentText()
+    for topic in toppic_notes:
+        page, note_id, title = topic
+        engine.prompt_exam(note_id, doc)
+        engine.next_step()
+
+    doc.save(name);
+
+    engine.exit()
+    
+except KeyboardInterrupt:
+    engine.exit()
